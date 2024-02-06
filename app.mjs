75c35bc9d11862c46794f7fc1d1f1cd2b5579ad6
unknown <joaoalbertobraun@gmail.com>
2024-02-06 19:24:42 -0300

    first commit

---
+++ app.mjs
@@ -0,0 +1,18 @@
+import Express from "express";
+import indexRoute from "./routes/index.js";
+
+const port = process.env.PORT || 10000;
+const app = Express();
+
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+app.use(Express.static("public"));
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.use("/", indexRoute);
+
+app.listen(port, () => {
+	console.log(`Server initialized on localhost:${port}`);
+});
